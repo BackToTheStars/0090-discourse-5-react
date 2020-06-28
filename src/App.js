eba6965b05e8ada1699c2dd1dbd3aeff3953754e
BackToTheStars <nikgrigoryev@gmail.com>
2020-06-27 21:43:13 -0700

    Template updated

--- src/App.js
+++ src/App.js
@@ -8,10 +8,21 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
   return (
     <div>
-
+      <div className="row">
+        <div className="col">
+          1 of 3
+        </div>
+        <div className="col-5">
+          2 of 3 (wider)
+        </div>
+        <div className="col">
+          3 of 3
+        </div>
+      </div>
     </div>
   );
 }
